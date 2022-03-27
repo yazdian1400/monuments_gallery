c9e91638e6669f8cc0e66a8de4faa3ac4fe96f61
Maedeh <yazdian.oul@gmail.com>
2022-03-27 12:59:23 +0430

    show an image in profile fragment by glide

--- app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
 import ir.homework.monuments_gallery.databinding.FragmentProfileBinding
 
@@ -21,5 +22,13 @@ class ProfileFragment : Fragment() {
     ): View? {
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(requireActivity())
+            .load(R.drawable.profile1)
+            .into(binding.ivProfile)
     }
 }
