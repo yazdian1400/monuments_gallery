0c01d8a5584b58fb956107639e7a8974dcabdb76
Maedeh <yazdian.oul@gmail.com>
2022-03-27 21:59:21 +0430

    add circular feature to profile image

--- app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
 import ir.homework.monuments_gallery.databinding.FragmentProfileBinding
 
@@ -29,6 +30,7 @@ class ProfileFragment : Fragment() {
 
         Glide.with(requireActivity())
             .load(R.drawable.profile1)
+            .circleCrop()
             .into(binding.ivProfile)
     }
 }
