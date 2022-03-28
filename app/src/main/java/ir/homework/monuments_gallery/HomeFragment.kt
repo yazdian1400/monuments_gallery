18fa201dfa0e2561d8a69b617e51388864a64533
Maedeh <yazdian.oul@gmail.com>
2022-03-28 21:13:42 +0430

    add selecting the number of images to setting

--- app/src/main/java/ir/homework/monuments_gallery/HomeFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/HomeFragment.kt
@@ -1,15 +1,19 @@
 package ir.homework.monuments_gallery
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,16 @@ class HomeFragment : Fragment() {
     ): View? {
     binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
     return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val numImage = sharedPreferences.getString("num_image", "4")
+        Toast.makeText(requireContext(), numImage, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
--- app/src/main/java/ir/homework/monuments_gallery/SettingFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/SettingFragment.kt
@@ -1,15 +1,20 @@
 package ir.homework.monuments_gallery
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
 import ir.homework.monuments_gallery.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
     lateinit var binding: FragmentSettingBinding
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,5 +25,20 @@ class SettingFragment : Fragment() {
     ): View? {
         binding = FragmentSettingBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNumSubmit.setOnClickListener{
+            val editor = sharedPreferences.edit()
+            editor.putString("num_image", binding.etNumImage.text.toString())
+            editor.apply()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
