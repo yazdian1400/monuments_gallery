69af46b77d65957f57bdb36cf895b443278f9437
Maedeh <yazdian.oul@gmail.com>
2022-03-29 11:38:09 +0430

    add setVisibilityOfItems function to Home

--- app/src/main/java/ir/homework/monuments_gallery/HomeFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/HomeFragment.kt
@@ -3,17 +3,21 @@ package ir.homework.monuments_gallery
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
     lateinit var sharedPreferences: SharedPreferences
+    private val itemList = mutableListOf<ConstraintLayout>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +33,32 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        itemList.clear()
+        itemList.add(binding.cl1)
+        itemList.add(binding.cl2)
+        itemList.add(binding.cl3)
+        itemList.add(binding.cl4)
+        itemList.add(binding.cl5)
+        itemList.add(binding.cl6)
         val numImage = sharedPreferences.getString("num_image", "4")
+        setVisibilityOfItems(numImage)
         Toast.makeText(requireContext(), numImage, Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun setVisibilityOfItems(numImage: String?) {
+        if (numImage != null) {
+            val num = numImage.toInt()
+            if (num in 1..6){
+                for (i in 0 until num) {
+                    itemList[i].isVisible = true
+                }
+                for (i in num..5) {
+                    itemList[i].isVisible = false
+                }
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
