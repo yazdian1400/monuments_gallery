147d4a4bf2ffa55b4abd4cba48061243c1eb584a
Maedeh <yazdian.oul@gmail.com>
2022-04-01 11:40:21 +0430

    add editing profile

--- app/src/main/java/ir/homework/monuments_gallery/EditProfileFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/EditProfileFragment.kt
@@ -1,5 +1,7 @@
 package ir.homework.monuments_gallery
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import ir.homework.monuments_gallery.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
     lateinit var binding: FragmentEditProfileBinding
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +23,51 @@ class EditProfileFragment : Fragment() {
     ): View? {
         binding = FragmentEditProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSave.setOnClickListener{
+            if (areAllFieldsFilled()) {
+                val editor = sharedPreferences.edit()
+                editor.putString("fullName", binding.etFullname.text.toString())
+                editor.putString("email", binding.etEmail.text.toString())
+                editor.putString("phoneNumber", binding.etTel.text.toString())
+                editor.putString("address", binding.etAddress.text.toString())
+                editor.putString("nationalCode", binding.etNationalCode.text.toString())
+                editor.putString("showProfile", binding.switchShowProfile.isChecked)
+                editor.apply()
+            }
+            showErrorForFilling()
+        }
+    }
+
+    private fun showErrorForFilling() {
+        if (binding.etFullname.text.isNullOrBlank())
+            binding.etFullname.error = "Please, Enter your full name."
+        if (binding.etNationalCode.text.isNullOrBlank())
+            binding.etNationalCode.error = "Please, Enter your national code."
+        if (binding.etTel.text.isNullOrBlank())
+            binding.etTel.error = "Please, Enter your phone number."
+        if (binding.etAddress.text.isNullOrBlank())
+            binding.etAddress.error = "Please, Enter your address."
+        if (binding.etEmail.text.isNullOrBlank())
+            binding.etEmail.error = "Please, Enter your email address."
+    }
+
+    private fun areAllFieldsFilled(): Boolean {
+        if (binding.etFullname.text.isNullOrBlank())    return false
+        if (binding.etNationalCode.text.isNullOrBlank())    return false
+        if (binding.etTel.text.isNullOrBlank())    return false
+        if (binding.etAddress.text.isNullOrBlank())    return false
+        if (binding.etEmail.text.isNullOrBlank())    return false
+
+        return true
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
--- app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package ir.homework.monuments_gallery
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
@@ -12,6 +15,7 @@ import ir.homework.monuments_gallery.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     lateinit var binding: FragmentProfileBinding
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,5 +36,18 @@ class ProfileFragment : Fragment() {
             .load(R.drawable.profile1)
             .circleCrop()
             .into(binding.ivProfile)
+
+        val fullName = sharedPreferences.getString("fullName", "")
+        val email = sharedPreferences.getString("email", "")
+        val phoneNumber = sharedPreferences.getString("phoneNumber", "")
+        val address = sharedPreferences.getString("address", "")
+        val nationalCode = sharedPreferences.getString("nationalCode", "")
+
+        Toast.makeText(requireContext(), fullName + email + phoneNumber + address + nationalCode,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
