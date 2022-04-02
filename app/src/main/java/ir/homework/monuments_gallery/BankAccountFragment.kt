0621c4b2c524bda89f873873cb1dc2c99e6e962a
Maedeh <yazdian.oul@gmail.com>
2022-04-02 08:31:07 +0430

    add BankAccount fragment

---
+++ app/src/main/java/ir/homework/monuments_gallery/BankAccountFragment.kt
@@ -0,0 +1,25 @@
+package ir.homework.monuments_gallery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ir.homework.monuments_gallery.databinding.FragmentBankAccountBinding
+import ir.homework.monuments_gallery.databinding.FragmentProfileBinding
+
+class BankAccountFragment : Fragment() {
+    lateinit var binding: FragmentBankAccountBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBankAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
