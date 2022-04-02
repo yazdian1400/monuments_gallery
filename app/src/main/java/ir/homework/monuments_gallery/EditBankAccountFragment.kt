9b71fe3ec9897c7bd43a596a53e1ab6119b6c006
Maedeh <yazdian.oul@gmail.com>
2022-04-02 18:52:31 +0430

    save data in shared preferences in editBankAccount fragment

--- app/src/main/java/ir/homework/monuments_gallery/EditBankAccountFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/EditBankAccountFragment.kt
@@ -1,5 +1,7 @@
 package ir.homework.monuments_gallery
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +12,7 @@ import ir.homework.monuments_gallery.databinding.FragmentEditBankAccountBinding
 
 class EditBankAccountFragment : Fragment() {
     lateinit var binding: FragmentEditBankAccountBinding
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +25,43 @@ class EditBankAccountFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentEditBankAccountBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSaveBankAccount.setOnClickListener{
+            if (areAllFieldsFilled()) {
+                val editor = sharedPreferences.edit()
+                editor.putString("accountNumber", binding.etAccountNumber.text.toString())
+                editor.putString("cardNumber", binding.etCardNumber.text.toString())
+                editor.putString("shebaNumber", binding.etShebaNumber.text.toString())
+                editor.apply()
+                requireActivity().onBackPressed()
+            } else {
+                showErrorForFilling()
+            }
+        }
+    }
+
+    private fun showErrorForFilling() {
+        if (binding.etAccountNumber.text.isNullOrBlank())
+            binding.etAccountNumber.error = "Please, Enter your account number."
+        if (binding.etCardNumber.text.isNullOrBlank())
+            binding.etCardNumber.error = "Please, Enter your card number."
+        if (binding.etShebaNumber.text.isNullOrBlank())
+            binding.etShebaNumber.error = "Please, Enter your sheba number."
+    }
+
+    private fun areAllFieldsFilled(): Boolean {
+        if (binding.etAccountNumber.text.isNullOrBlank())    return false
+        if (binding.etCardNumber.text.isNullOrBlank())    return false
+        if (binding.etShebaNumber.text.isNullOrBlank())    return false
+        return true
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
