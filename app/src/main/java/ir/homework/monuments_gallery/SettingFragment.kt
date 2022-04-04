4998b4a9af24972aa5efd049283597a9d8162a00
Maedeh <yazdian.oul@gmail.com>
2022-04-04 17:44:18 +0430

    update layout of setting with seekbar

--- app/src/main/java/ir/homework/monuments_gallery/SettingFragment.kt
+++ app/src/main/java/ir/homework/monuments_gallery/SettingFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import ir.homework.monuments_gallery.databinding.FragmentHomeBinding
@@ -31,11 +32,36 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnNumSubmit.setOnClickListener{
-            val editor = sharedPreferences.edit()
-            editor.putString("num_image", binding.etNumImage.text.toString())
-            editor.apply()
+        val numItem = sharedPreferences.getString("num_image", "4")?.toInt()
+        if (numItem != null) {
+            binding.seekbarNum.progress = numItem
+            binding.tvNumItem.text = binding.seekbarNum.progress.toString()
         }
+
+        binding.seekbarNum.setOnSeekBarChangeListener(object:
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                binding.tvNumItem.text = binding.seekbarNum.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                val editor = sharedPreferences.edit()
+                editor.putString("num_image", binding.seekbarNum.progress.toString())
+                editor.apply()
+            }
+        })
+
+//        binding.btnNumSubmit.setOnClickListener{
+//            val editor = sharedPreferences.edit()
+//            editor.putString("num_image", binding.etNumImage.text.toString())
+//            editor.apply()
+//        }
 
         binding.btnEditProfile.setOnClickListener{
             findNavController().navigate(R.id.action_nav_setting_to_editProfileFragment)
@@ -50,3 +76,23 @@ class SettingFragment : Fragment() {
         sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
     }
 }
+
+//<LinearLayout
+//android:layout_width="match_parent"
+//android:layout_height="0dp"
+//android:layout_weight="1"
+//android:orientation="horizontal"
+//android:padding="8dp">
+//
+//<EditText
+//android:id="@+id/et_num_image"
+//android:layout_width="100dp"
+//android:layout_height="match_parent"
+//android:hint="4"
+//android:textSize="36sp" />
+//<Button
+//android:layout_width="100dp"
+//android:layout_height="match_parent"
+//android:id="@+id/btn_num_submit"
+//android:text="submit"/>
+//</LinearLayout>
